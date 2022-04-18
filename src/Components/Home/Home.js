3d3a83e05fa171ed8976942a334e88ccdb27089a
jleee14 <kjleee14@gmail.com>
2022-04-18 10:25:24 -0700

    add home styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -23,17 +23,27 @@ function Home(props) {
 				</div>
 			</div>
 			<div className="home-container">
-				<div className="home-image-container">Image placeholder</div>
-				<div className="home-blurb-container">
-					<div className="title-container">
-						<h2 className="title-header">
-							The Future of Social Sports Betting Is Here.
-						</h2>
+				<div className="home-content-container">
+					<div className="home-image-container">
+						<div className="image-container" id="first-image">
+							<img src="https://i.imgur.com/b1xH0ON.png" alt="dash-screen" />
+						</div>
+						<div className="image-container" id="second-image">
+							<img src="https://i.imgur.com/z5P6ZWY.png" alt="bet-screen" />
+						</div>
 					</div>
-					<div className="subtext-container">
-						<p className="subtext-header">
-							Find your favorite cappers. Copy them. Build your network. Blah.
-						</p>
+					<div className="home-blurb-container">
+						<div className="title-container">
+							<h2 className="title-header">
+								Find your niche, exploit it. Profit.
+							</h2>
+						</div>
+						<div className="subtext-container">
+							<p className="subtext-header">
+								eCapr provides integrated tooling and tracking for dedicated
+								gamblers.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
